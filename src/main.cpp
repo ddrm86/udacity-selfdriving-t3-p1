34526fbacccd16c19b615f16538ebde36cca4c44
David del Río Medina <ddrm86@gmail.com>
2017-08-14 18:25:04 +0200

    Add smooth lane keeping except for start
    Udacity walkthrough up to 39:00:
    
    - No collision avoidance.
    - Exceeds jerk at the beginning.

--- src/main.cpp
+++ src/main.cpp
@@ -163,39 +163,6 @@ vector<double> getXY(double s, double d, vector<double> maps_s, vector<double> m
 
 }
 
-void printVector(vector<double> vec) {
-  cout << "[";
-  for (int i=0; i<vec.size(); i++) {
-    if (i == (vec.size() - 1)) {
-      cout << vec[i];
-    } else {
-      cout << vec[i] << ", ";
-    }
-  }
-  cout << "]" << endl;
-}
-
-vector<double> JMT(vector< double> start, vector <double> end, double T)
-{
-    double i_0 = end[0] - (start[0] + start[1]*T + 0.5*start[2]*T*T);
-    double i_1 = end[1] - (start[1] + start[2]*T);
-    double i_2 = end[2] - start[2];
-    
-    VectorXd ind(3);
-    ind << i_0, i_1, i_2;
-    
-    MatrixXd coeffs(3,3);
-    coeffs << pow(T,3), pow(T,4), pow(T,5),
-              3*pow(T,2), 4*pow(T,3), 5*pow(T,4),
-              6*T, 12*pow(T,2), 20*pow(T,3);
-    
-    VectorXd sol = coeffs.inverse() * ind;
-    
-    return {start[0], start[1], 0.5*start[2], sol(0), sol(1), sol(2)};    
-}
-
-
-
 int main() {
   uWS::Hub h;
 
@@ -233,7 +200,10 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  int lane = 1;
+  double ref_vel = 49.5; //mph
+
+  h.onMessage([&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -277,25 +247,93 @@ int main() {
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-          	
-          	cout << "================" << endl;
-          	cout << "Car pos: " << car_x << ", " << car_y << endl;
-          	cout << "Car pos frenet: " << car_s << ", " << car_d << endl;
-          	cout << "Car yaw: " << car_yaw << endl;
-          	
-        	  double dist_inc = 0.2;
-         	  for (int i=1; i<50; i++) {
-         	    double next_s = car_s + dist_inc * i;          	  
-         	    vector<double> coords = getXY(next_s, car_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-         	    cout << "next_s: " << next_s << " coords: " << coords[0] << ", " << coords[1] << endl;
-         	    next_x_vals.push_back(coords[0]);
-         	    next_y_vals.push_back(coords[1]);
-         	  }
-          	
-          	printVector(next_x_vals);
-          	printVector(next_y_vals);
-          	
-          	cout << endl << "================" << endl;          	
+            int prev_size = previous_path_x.size();
+ 	
+            vector<double> ptsx;
+            vector<double> ptsy;
+            
+            double ref_x = car_x;
+            double ref_y = car_y;
+            double ref_yaw = deg2rad(car_yaw);
+
+            if (prev_size < 2) {
+              double prev_car_x = car_x - cos(car_yaw);
+              double prev_car_y = car_y - sin(car_yaw);
+
+              ptsx.push_back(prev_car_x);
+              ptsx.push_back(car_x);
+
+              ptsy.push_back(prev_car_y);
+              ptsy.push_back(car_y);
+            } else {
+              ref_x = previous_path_x[prev_size-1];
+              ref_y = previous_path_y[prev_size-1];
+
+              double ref_x_prev = previous_path_x[prev_size-2];
+              double ref_y_prev = previous_path_y[prev_size-2];
+              ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+              ptsx.push_back(ref_x_prev);
+              ptsx.push_back(ref_x);
+
+              ptsy.push_back(ref_y_prev);
+              ptsy.push_back(ref_y);
+            }
+
+            vector<double> next_wp0 = getXY(car_s+30,2+4*lane,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+            vector<double> next_wp1 = getXY(car_s+60,2+4*lane,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s+90,2+4*lane,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+
+            ptsx.push_back(next_wp0[0]);
+            ptsx.push_back(next_wp1[0]);
+            ptsx.push_back(next_wp2[0]);
+
+            ptsy.push_back(next_wp0[1]);
+            ptsy.push_back(next_wp1[1]);
+            ptsy.push_back(next_wp2[1]);
+
+            for (int i=0; i<ptsx.size(); i++) {
+              double shift_x = ptsx[i]-ref_x;
+              double shift_y = ptsy[i]-ref_y;
+
+              ptsx[i] = (shift_x *cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
+              ptsy[i] = (shift_x *sin(0-ref_yaw)+shift_y*cos(0-ref_yaw));
+            }
+
+            tk::spline s;
+
+            s.set_points(ptsx,ptsy);
+
+            for (int i=0; i<previous_path_x.size(); i++) {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+            }
+
+            double target_x = 30.0;
+            double target_y = s(target_x);
+            double target_dist = sqrt((target_x)*(target_x)+(target_y)*(target_y));
+            double x_add_on = 0;
+
+            for (int i=1; i<= 50-previous_path_x.size(); i++) {
+              double N = (target_dist/(.02*ref_vel/2.24));
+              double x_point = x_add_on+(target_x)/N;
+              double y_point = s(x_point);
+
+              x_add_on = x_point;
+      
+              double x_ref = x_point;
+              double y_ref = y_point;
+
+              // rotate back to normal after rotating it earlier
+              x_point = (x_ref *cos(ref_yaw)-y_ref*sin(ref_yaw));
+              y_point = (x_ref *sin(ref_yaw)+y_ref*cos(ref_yaw));
+
+              x_point += ref_x;
+              y_point += ref_y;
+
+              next_x_vals.push_back(x_point);
+              next_y_vals.push_back(y_point);
+            }
           	
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
