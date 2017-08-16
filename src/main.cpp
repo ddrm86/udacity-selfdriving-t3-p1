5d35259fb4d9f265b3f8962ef4bc3f9502c26a9c
David del Río Medina <ddrm86@gmail.com>
2017-08-16 19:07:26 +0200

    Add smooth lane keeping
    Udacity walkthrough up to 51:30:
    
      - Colision avoidance by setting fixed slow speed.
      - Avoids jerk at the beginning or when changing ref velocity.

--- src/main.cpp
+++ src/main.cpp
@@ -201,7 +201,7 @@ int main() {
   }
 
   int lane = 1;
-  double ref_vel = 49.5; //mph
+  double ref_vel = 0.0; //mph
 
   h.onMessage([&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -248,6 +248,34 @@ int main() {
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
             int prev_size = previous_path_x.size();
+ 	
+ 	          if (prev_size > 0) {
+ 	            car_s = end_path_s;
+ 	          }
+ 	          
+ 	          bool too_close = false;
+ 	          
+ 	          for (int i=0; i < sensor_fusion.size(); i++) {
+ 	            float d = sensor_fusion[i][6];
+ 	            if (d < (2+4*lane+2) && d > (2+4*lane-2)) {
+ 	              double vx = sensor_fusion[i][3];
+ 	              double vy = sensor_fusion[i][4];
+ 	              double check_speed = sqrt(vx*vx+vy*vy);
+ 	              double check_car_s = sensor_fusion[i][5];
+ 	              
+ 	              check_car_s += ((double)prev_size*.02*check_speed);
+ 	              
+ 	              if ((check_car_s > car_s) && ((check_car_s-car_s) < 30)) {
+ 	                too_close = true;
+ 	              }
+ 	            }
+ 	          }
+ 	
+ 	          if (too_close) {
+ 	            ref_vel -= .224; //aprox 5m/s
+ 	          } else if (ref_vel < 49.5) {
+ 	            ref_vel += .224;
+ 	          }
  	
             vector<double> ptsx;
             vector<double> ptsy;
